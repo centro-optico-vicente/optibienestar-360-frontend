bbaaf628850ea29fc224766f62b51a623ba7bd9e
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2026-09-10 00:48:19 -0400

    feat: Start App with script (#136)

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# show errors and exit on error
+set -euo pipefail
+
+
+# nvm source dir
+export NVM_DIR="$HOME/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  \. "$NVM_DIR/nvm.sh"
+else
+  echo "ERROR: nvm not found at $NVM_DIR/nvm.sh" >&2
+  exit 1
+fi
+
+
+# nvm use 22 && pnpm clear && pnpm dev
+
+nvm use 22;
+
+pnpm clear;
+
+pnpm dev;
